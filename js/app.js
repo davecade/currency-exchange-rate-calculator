a60a2a1b46db99daa204f2e881e2e75eadc5a516
Dave <dave.cadelina@outlook.com>
2020-11-08 00:23:47 +1100

    currency calculation functional

--- js/app.js
+++ js/app.js
@@ -6,36 +6,35 @@ const amountTwoEl = document.querySelector('#amount-two')
 const swapEl = document.querySelector('#swap')
 const rateEl = document.querySelector('#rate')
 
-//-- Event Listeners
-
-currencyOneEl.addEventListener('change', () => {
-    console.log(currencyOneEl.value)
-})
-
-currencyTwoEl.addEventListener('change', () => {
-    console.log(currencyTwoEl.value)
-})
-
-amountOneEl.addEventListener('input', () => {
-
-})
-
-amountTwoEl.addEventListener('input', () => {
-    
-})
-
-swapEl.addEventListener('click', () => {
-
-})
 
 //-- Render the DOM
 const renderAll = () => {
     const currencyOne = currencyOneEl.value
+    const currencyTwo = currencyTwoEl.value
 
     // -- API: https://www.exchangerate-api.com/
     fetch(`https://v6.exchangerate-api.com/v6/66948f4679114c5deb21ca27/latest/${currencyOne}`)
     .then(response => (response.json()))
-    .then(data => console.log(data))
+    .then(data => {
+
+        // -- Render exchange rate
+        let currentRate = data.conversion_rates[currencyTwo]
+        rateEl.innerHTML = `1 ${currencyOne} = ${currentRate} ${currencyTwo}`;
+
+        // -- Calculate currency amount
+        amountTwoEl.value = (currentRate * amountOneEl.value).toFixed(2);
+    })
+
+    
+    
 }
+
+
+//-- Event Listeners
+currencyOneEl.addEventListener('change', renderAll)
+currencyTwoEl.addEventListener('change', renderAll)
+amountOneEl.addEventListener('input', renderAll)
+amountTwoEl.addEventListener('input', renderAll)
+swapEl.addEventListener('click', renderAll)
 
 renderAll()
